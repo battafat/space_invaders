c6702b1ddd55f522385e739a3fe510bc49129fd6
timothy battafarano <tim@deepsprings.edu>
2024-05-22 18:18:58 -0500

    refactored shiftAliensRight and shiftAliensLeft to take List<String> row argument instead of <String> key

--- bin/alien.dart
+++ bin/alien.dart
@@ -48,32 +48,33 @@ Map<String, List<String>> aliensTraverseXaxis(int iterations, List rows) {
 }
 
 Map<String, List<String>> shiftAliensRight(
-    Map<String, List<String>> newBoard1, String rowKey) {
+    Map<String, List<String>> newBoard1, List<String> row) {
   var newBoard = newBoard1;
   // TODO: refactor i value to not include border.
   // Todo: refactor i value into variable. Currently hard to read
-  for (int i = newBoard[rowKey]!.length - 1; i >= 0; i--) {
+  for (int i = row.length - 1; i >= 0; i--) {
     if (i == 1) {
-      newBoard[rowKey]![i] = Board.space;
+      row[i] = Board.space;
       continue;
-    } else if (newBoard[rowKey]![i] == Board.border) {
+    } else if (row[i] == Board.border) {
       continue;
     } else {
-      newBoard[rowKey]![i] = newBoard[rowKey]![i - 1];
+      row[i] = row[i - 1];
     }
   }
   return newBoard;
 }
 
-Map<String, List<String>> shiftAliensLeft(board, row) {
-  for (int i = 0; i < board[row].length; i++) {
-    if (i == board[row].length - 2) {
-      board[row][i] = Board.space;
+Map<String, List<String>> shiftAliensLeft(
+    Map<String, List<String>> board, List<String> row) {
+  for (int i = 0; i < row.length; i++) {
+    if (i == row.length - 2) {
+      row[i] = Board.space;
       continue;
-    } else if (board[row][i] == Board.border) {
+    } else if (row[i] == Board.border) {
       continue;
     } else {
-      board[row][i] = board[row][i + 1];
+      row[i] = row[i + 1];
     }
   }
   return board;
