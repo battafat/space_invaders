5d65eaf719b6bb238a6733637829db1f0f23f8d4
timothy battafarano <tim@deepsprings.edu>
2024-05-26 16:25:37 -0500

    written and passed: aliensShiftRight aliens start row[2]

--- test/alien_test.dart
+++ test/alien_test.dart
@@ -5,9 +5,18 @@ import '../bin/alien.dart';
 import '../bin/board.dart';
 
 void main (){
+  // alien tests start at row[0] and end at row[row.length - 1] because:
+  // the border '|' occupies row[0] and row[row.length-1].
+  // So leftmost possible alien coordinate is row[1].
+  // And rightmost possible alien coordinate is row[row.len - 2].
 
-  
-  test('alienShiftRight aliens start row[3]', (){
+  test('aliensShiftRight aliens start row[2]', () {
+    final alien = Alien();
+    final alienRow1 = ['|', ' ', '@', '@', '@', ' ', ' ', ' ', '|'];
+    const updatedRow = ['|', ' ', ' ', '@', '@', '@', ' ', ' ', '|'];
+    expect(alien.shiftAliensRight(alienRow1), updatedRow);
+  });
+  test('alienShiftRight aliens start row[3]', () {
     final alien = Alien();
     final boardClass = Board();
     final board = boardClass.board;
