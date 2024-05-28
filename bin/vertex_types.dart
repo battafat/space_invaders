13fa4b6334ec5b5000a0d624e58d0af68682f875
timothy battafarano <tim@deepsprings.edu>
2024-05-28 12:20:27 -0500

    commented out entire vertex_types.dart file

--- bin/vertex_types.dart
+++ bin/vertex_types.dart
@@ -1,48 +1,50 @@
-enum VertexType {
-  player, 
-  alien, 
-  blankSpace,
-  border,
-  }
+// import 'board.dart';
 
-class Vertex {
-  const Vertex({
-    required this.vertexType
-  });
-  final VertexType vertexType;
-   String assignEnum(){
-    if (VertexType == alien){
-      return "@"; 
-    }
-   }
-}
-String selectVertexType(){
-  switch (pos) {
-    case VertexType.alien:
-      return "A";
-}
-}
-class Alien {
-  const Alien({
-    required this.symbol,
-    required this.direction
-});
+// enum VertexType {
+//   player, 
+//   alien, 
+//   blankSpace,
+//   border,
+//   }
 
-  final String symbol;
-  final String direction;
-  final VertexType vertexType = VertexType.alien;
-}
+// class Vertex {
+//   const Vertex({
+//     required this.vertexType
+//   });
+//   final VertexType vertexType;
+//    String assignEnum(){
+//     if (VertexType == alien){
+//       return "@"; 
+//     }
+//    }
+// }
+// String selectVertexType(){
+//   switch (pos) {
+//     case VertexType.alien:
+//       return "A";
+// }
+// }
+// // class Alien {
+// //   const Alien({
+// //     required this.symbol,
+// //     required this.direction
+// // });
 
-class Player {
-  const Player({
-    required this.symbol,
-  });
-  final String symbol;
-}
+// //   final String symbol;
+// //   final String direction;
+// //   final VertexType vertexType = VertexType.alien;
+// // }
 
-class BlankSpace {
-  const BlankSpace({
-    required this.symbol,
-  });
-  final String symbol;
-}
+// // class Player {
+// //   const Player({
+// //     required this.symbol,
+// //   });
+// //   final String symbol;
+// // }
+
+// // class BlankSpace {
+// //   const BlankSpace({
+// //     required this.symbol,
+// //   });
+// //   final String symbol;
+// // }
