e32ac60ac9a9f32e753a71f9d342297937ebf86d
timothy battafarano <tim@deepsprings.edu>
2024-06-20 17:14:52 -0500

    refactored back to timer loop. works now. removed extraneous comments

--- bin/space_invaders.dart
+++ bin/space_invaders.dart
@@ -21,6 +21,7 @@ void main() async {
   // TODO: add reset command at beginning to make sure terminal is clear
   var changeDirection = false;
   final player = Player();
+  final alienCount = columns ~/ 3;
   var alienPositions = [Point(0,1), Point(0,2), Point(0,3)];
   
   stdout.flush();
@@ -32,45 +33,40 @@ void main() async {
   // StreamSubscription<List<int>> subscriber1;
   StreamSubscription<List<int>> subscriber1;
   
-    stdinStreamSubscription = stdin.listen((event) {
-      // TODO: add if statment for escape code: 27(?)
-      if (event.length >= 3 && event[1] == 91) { 
-            switch (event[2]) {
-              // up arrow pressed
-              case 65:
-                event = [65];
-                break;
-              // down arrow pressed
-              case 66:
-                event = [66];
-                break;
-                // right arrow pressed
-              case 67:
-                event = [67];
-                break;
-                // left arrow pressed
-              case 68:
-                event = [68];
-                break;
-              default:
-                print("Unknown escape sequence: $event");
-            }
-        print('57 $event');
+  stdinStreamSubscription = stdin.listen((event) {
+    // TODO: add if statment for escape code: 27(?)
+    // TODO: refactor this switch statement into a function
+    if (event.length >= 3 && event[1] == 91) { 
+          switch (event[2]) {
+            // up arrow pressed
+            case 65:
+              event = [65];
+              break;
+            // down arrow pressed
+            case 66:
+              event = [66];
+              break;
+              // right arrow pressed
+            case 67:
+              event = [67];
+              break;
+              // left arrow pressed
+            case 68:
+              event = [68];
+              break;
+            default:
+              print("Unknown escape sequence: $event");
+          }
       }
       // streamController.add(event); // Send the update signal
       // print('event $event');
       final KeyTypes key = KeyTypes.fromValue(event);
-      player.handlePlayerMove(key, playerPosition);
+      playerPosition = player.handlePlayerMove(key, playerPosition);
       print('64 $key');
       streamController.add(event);
     });
-  // Timer.periodic(Duration(seconds: 3), (Timer timer) async {
-  while (true){
-    subscriber1 = streamController.stream.listen((event) {
-      final KeyTypes key = KeyTypes.fromValue(event);
-      playerPosition = player.handlePlayerMove(key, playerPosition);
-      print('71 key: $key');
-     });
+  Timer.periodic(Duration(seconds: 1), (Timer timer) async {
+
     await Future.delayed(Duration(milliseconds: 100));
     for (var row = 0; row < rows; row++){
       for (var column = 0; column < columns; column++){
@@ -99,24 +95,8 @@ void main() async {
         direction = right;
       }
     }
-    // subscriber1 = streamController.stream.listen((event) {
-    //   print('68 $event');
-    //   final key = KeyTypes.fromValue(event);
-    //   print('70 $key');
-    //   playerPosition = player.handlePlayerMove(key, playerPosition);
+    });
 
-    //   // streamController.add(event);
-    // });
-    
-    subscriber1.cancel();
-
-    // subscriber1.cancel();
-  } 
-  // });
-  
-
-  
- 
   // makes sure the buffer is flushed so that terminal
   // doesn't print previous game's output
   stdout.flush();
