bc9f8d1d529999cc07e19a9fc2c35488d77400fc
timothy battafarano <tim@deepsprings.edu>
2024-06-30 19:24:07 -0500

    refactored arrow key switch statement into processUserInput function

--- bin/space_invaders.dart
+++ bin/space_invaders.dart
@@ -25,6 +25,32 @@ Future<List<Point<int>>> compileAlienPositions(int rows, int columns) async{
   }
   return alienPositions;
 }
+List<int> processUserInput(List<int> event){
+  if (event.length >= 3 && event[1] == 91) {
+    switch (event[2]) {
+      // up arrow pressed
+      case 65:
+        event = [65];
+        break;
+      // down arrow pressed
+      case 66:
+        event = [66];
+        break;
+      // right arrow pressed
+      case 67:
+        event = [67];
+        break;
+      // left arrow pressed
+      case 68:
+        event = [68];
+        break;
+      default:
+        print("Unknown escape sequence: $event");
+    }
+  }
+  return event;
+}
+
 void main() async {
   var direction = right;
   // TODO: add reset command at beginning to make sure terminal is clear
@@ -40,41 +66,22 @@ void main() async {
   StreamController<List<int>> streamController = StreamController<List<int>>.broadcast();
   
   stdinStreamSubscription = stdin.listen((event) {
-    // TODO: add if statment for escape code: 27(?)
-    // TODO: refactor this switch statement into a function
-    if (event.length >= 3 && event[1] == 91) { 
-          switch (event[2]) {
-            // up arrow pressed
-            case 65:
-              event = [65];
-              break;
-            // down arrow pressed
-            case 66:
-              event = [66];
-              break;
-              // right arrow pressed
-            case 67:
-              event = [67];
-              break;
-              // left arrow pressed
-            case 68:
-              event = [68];
-              break;
-            default:
-              print("Unknown escape sequence: $event");
-          }
-      }
-      final KeyTypes key = KeyTypes.fromValue(event);
-      playerPosition = player.handlePlayerMove(key, playerPosition);
-      streamController.add(event);
-    });
+    // TODO: write tests for this function
+    event = processUserInput(event);
+    final KeyTypes key = KeyTypes.fromValue(event);
+    playerPosition = player.handlePlayerMove(key, playerPosition);
+    streamController.add(event);
+  });
 
   Timer.periodic(Duration(milliseconds: 700), (Timer timer) async {
     late List<List<String>> board = List.generate((rows), (_) => List.filled(columns, ' '));
     await Future.delayed(Duration(milliseconds: 100));
     // clear the screen after displaying the board
+    // TODO: refactor into clearScreen function
+    // TODO: possibly write tests for clearScreen function?
     print('\x1B[2J\x1B[H');
-    
+    // TODO: refactor this loop into function updateBoard
+    // TODO: write tests for the function
     for (var row = 0; row < rows; row++){
       for (var column = 0; column < columns; column++){
         if (alienPositions.contains(Point(row,column))){
@@ -97,6 +104,8 @@ void main() async {
       }
     }
     // display the board after each update
+    // TODO: refactor this into displayScreen function
+    // TODO: write a test for this function?
     for (var x = 0; x < board.length; x++){
       print(board[x].join());
     }
@@ -104,6 +113,8 @@ void main() async {
     // enough to see between updates
     sleep(Duration(milliseconds: 500));
     // await Future.delayed(Duration(seconds: 2));
+    // TODO: refactor this if statement into updateDirection?
+    // TODO: if refactored into function, write tests
     if (changeDirection == true){
       if (direction == right){
         direction = left;
@@ -113,7 +124,8 @@ void main() async {
       }
       changeDirection = false;
     }
-  
+    // TODO: refactor into function updateAlienPositions
+    // TODO: write tests for function
     for (var i = 0; i < alienPositions.length; i++){
       alienPositions[i] = Point(alienPositions[i].x, alienPositions[i].y + direction);
     }
