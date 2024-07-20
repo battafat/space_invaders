a00b05071dcc01073c6439d4f2fe388590eac556
timothy battafarano <tim@deepsprings.edu>
2024-07-19 20:19:42 -0500

    refactored Timer loop into function within new GameLoop class: gameLoop(controlBoard, alien, player)

---
+++ bin/game_loop.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:io';
+import 'alien.dart';
+import 'board.dart';
+import 'player.dart';
+
+class GameLoop {
+  void runGameLoop(Board controlBoard, Alien alien, Player player){
+    Timer.periodic(Duration(milliseconds: 700), (Timer timer) async {
+      await Future.delayed(Duration(milliseconds: 100));
+      // clear the screen after displaying the boardState
+      // TODO: possibly write tests for clearScreen function?
+      controlBoard.clearScreen();
+      controlBoard.updateBoardState(
+          alien.alienPositions, player.playerPosition);
+      // display the boardState after each update
+      // TODO: write a test for this function?
+      controlBoard.printBoardState();
+      // sleep keeps the boardState visible long enough to see between updates
+      sleep(Duration(milliseconds: 500));
+      if (controlBoard.changeDirection == true) {
+        controlBoard.reverseDirection();
+        //reset changeDirection
+        controlBoard.changeDirection = false;
+      }
+      alien.updateAlienPositions(alien.alienPositions, controlBoard.direction);
+    });
+  }
+}
--- bin/space_invaders.dart
+++ bin/space_invaders.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'alien.dart';
 import 'board.dart';
+import 'game_loop.dart';
 import 'key_types.dart';
 import 'player.dart';
 import 'user_input.dart';
@@ -10,37 +11,16 @@ late final StreamSubscription<List<int>> stdinStreamSubscription;
 
 void main() async {
   // TODO: add reset command at beginning to make sure terminal is clear
+  final gameLoop = GameLoop();
   final player = Player();
   final controlBoard = Board();
   final alien = Alien();
   final userInput = UserInput();
 
   stdout.flush();
-  // TODO: write startUserInput()
   userInput.startUserInput(player);
-  
-  
   // TODO: Make this it's own gameLoop() ?
-  Timer.periodic(Duration(milliseconds: 700), (Timer timer) async {
-    await Future.delayed(Duration(milliseconds: 100));
-    // clear the screen after displaying the boardState
-    // TODO: possibly write tests for clearScreen function?
-    controlBoard.clearScreen();
-    controlBoard.updateBoardState(alien.alienPositions, player.playerPosition);
-    // display the boardState after each update
-    // TODO: write a test for this function?
-    controlBoard.printBoardState();
-    // sleep keeps the boardState visible long enough to see between updates
-    sleep(Duration(milliseconds: 500));
-    if (controlBoard.changeDirection == true) {
-      controlBoard.reverseDirection();
-      //reset changeDirection
-      controlBoard.changeDirection = false;
-    }
-    alien.updateAlienPositions(alien.alienPositions, controlBoard.direction);
-    
-  });
-
+  gameLoop.runGameLoop(controlBoard, alien, player);
   // makes sure the buffer is flushed so that terminal
   // doesn't print previous game's output
   //TODO: add some kind of reset? that or endGame function
